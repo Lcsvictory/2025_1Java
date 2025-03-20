6f561510b366f076654d77eaff48c1b0d865238e
Lcsvictory <yuko135z@gmail.com>
2025-03-20 20:02:03 +0900

    homework & README.md/.gitignore UPDATE

--- week03/src/week03/ProductPriceHomeWork.java
+++ week03/src/week03/ProductPriceHomeWork.java
@@ -6,16 +6,18 @@ public class ProductPriceHomeWork {
 		int milk = 4500; int milkCnt = 1;
 		int carrot = 1000; int carrotCnt = 3;
 		int snack = 1500; int snackCnt = 2;
-		double discountPer = 10;
+		double rate = 25;
 		//야채품목제외 할인율 25%
 		
 		int total = (milk * milkCnt) + (carrot*carrotCnt) + (snack*snackCnt); 
 		System.out.printf("할인 전 상품 가격 %d원\n", total);
 		
-		double discountPrice = (total * discountPer) / 100;
+		int totalVege = carrot*carrotCnt;
+		int totalNoneVege = total - totalVege;
+		double discountPrice = (totalNoneVege * rate) / 100;
 		System.out.printf("할인 금액 : %.1f원\n", discountPrice);
 		
-		double finalTotal = total - discountPrice;
+		double finalTotal = totalVege + (totalNoneVege - discountPrice) ;
 		System.out.printf("최종 결제 금액 : %.1f원\n", finalTotal);
 		
 		
